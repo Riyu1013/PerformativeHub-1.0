85ea9a64884ae1d30fb202b180b7ecc1ae71613c
FredMoriam <rimandofredrik@gmail.com>
2025-12-01 13:17:42 +0800

    WIP: music page

---
+++ script/data-music.js
@@ -0,0 +1,38 @@
+const SONGS = [
+  {
+    "title": "Bags",
+    "artist": "Clairo",
+    "coverFilename": "clairo.png",
+    "audioFilename": "clairo-bags.mp3"
+  },
+  {
+    "title": "Sofia",
+    "artist": "Clairo",
+    "coverFilename": "clairo.png",
+    "audioFilename": "clairo-sofia.mp3"
+  },
+  {
+    "title": "The Apartment We Won't Share",
+    "artist": "Niki",
+    "coverFilename": "niki.png",
+    "audioFilename": "niki-the-apartment-we-wont-share.mp3"
+  },
+  {
+    "title": "I Bet On Losing Dogs",
+    "artist": "Mitski",
+    "coverFilename": "mitski.jpg",
+    "audioFilename": "mitski-i-bet-on-losing-dogs.mp3"
+  },
+  {
+    "title": "Waiting Room",
+    "artist": "Phoebe Bridgers",
+    "coverFilename": "phoebe-bridgers.jpg",
+    "audioFilename": "phoebe-bridgers-waiting-room.mp3"
+  },
+  {
+    "title": "Olivia Dean",
+    "artist": "Dive",
+    "coverFilename": "olivia-dean.jpg",
+    "audioFilename": "olivia-dean-dive.mp3"
+  },
+];
